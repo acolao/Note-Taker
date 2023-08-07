185a1d3a3f7ddb22e026b4e324cd95f86ebcca07
Alison Colao <alison.siwek@gmail.com>
2023-08-07 14:38:42 -0700

    added html and api routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const notesRoute = require("./routes/notes");
 const apiRoute = require("./routes/app");
+const htmlRoute = require("./routes/htmlRoutes")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api', apiRoute);
 app.use('/', notesRoute);
+app.use('/html', htmlRoute);
 
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`)});
